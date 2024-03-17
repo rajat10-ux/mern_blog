44c700c88f2299087a0de2763551c9ba7bc93a99
rajat10-ux <rajatko614@gmail.com>
2024-03-17 20:52:40 +0530

    connect to backend

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import  express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+        console.log('connected');
+})
+.catch((err)=>{
+        console.log(err);
+})
 const app=express();
 app.listen(4000,()=>{
         console.log(`server running on 4000`);
