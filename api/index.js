1ef373bfa7f51d79c6a1e967a81bdf8f9057e1da
rajat10-ux <rajatko614@gmail.com>
2024-03-17 21:14:35 +0530

    create a api route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
 import  express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 mongoose
 .connect(process.env.MONGO)
-.then(()=>{
-        console.log('connected');
-})
-.catch((err)=>{
-        console.log(err);
-})
+// .then(()=>{
+//         console.log('connected');
+// })
+// .catch((err)=>{
+//         console.log(err);
+// })
 const app=express();
 app.listen(4000,()=>{
         console.log(`server running on 4000`);
 })
+app.use('/api/user',userRoutes);
---
+++ api/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+.then(()=>{
+//         console.log('connected');
+// })
+// .catch((err)=>{
+//         console.log(err);
+// })
